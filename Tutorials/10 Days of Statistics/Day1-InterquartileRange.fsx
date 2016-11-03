b7f79d35e5183321232f2c6f13e7e178d8d7492b
Jeremy Kruer <Jeremy.Kruer@adp.com>
2016-11-03 16:33:43 -0400

    Day 1 Statistics Quartiles and Interquartile Range

---
+++ Tutorials/10 Days of Statistics/Day1-InterquartileRange.fsx
@@ -0,0 +1,63 @@
+﻿open System
+
+let calculateEvenMedianAndIndex (array:float[]) =
+    let maxIndex =  float (Array.length array - 1)
+    let index = maxIndex / 2.
+    let lower = floor index |> int
+    let upper = ceil index |> int
+    let median = (Array.get array lower + Array.get array upper) / 2.
+    (median, index)
+    
+let calculateOddMedianAndIndex array = 
+    let index = Array.length array / 2
+    let median = Array.get array index
+    (median, index |> float)
+
+let calculateMedianAndIndex array =
+    match Array.length array % 2 = 0 with
+    | true -> calculateEvenMedianAndIndex array
+    | false -> calculateOddMedianAndIndex array
+
+let splitBySpace (input:string) =
+    input.Split ' '
+
+let readInputs _ = 
+    Console.ReadLine () 
+    |> splitBySpace 
+    |> Array.map(int)
+
+
+let main _ = 
+    let lengthOfInput = Console.ReadLine () |> int
+    let numbers = readInputs ()
+    let frequencies = readInputs ()
+
+    let sortedArray =
+        Array.map2(Array.create) frequencies numbers
+        |> Array.concat
+        |> Array.sort
+        |> Array.map(float)
+
+    let q2, index = calculateMedianAndIndex sortedArray
+    let q1, _ =
+        sortedArray
+        |> Array.mapi(fun i item -> 
+            match float i < index with
+            | true -> Some item
+            | false -> None)
+        |> Array.choose(id)
+        |> calculateMedianAndIndex
+    let q3, _ =
+        sortedArray
+        |> Array.mapi(fun i item -> 
+            match float i > index with
+            | true -> Some item
+            | false -> None)
+        |> Array.choose(id)
+        |> calculateMedianAndIndex
+
+    printfn "%.1f" (q3 - q1)
+    
+    0 // return an integer exit code
+
+main ()
---
+++ Tutorials/10 Days of Statistics/Day1-Quartiles.fsx
@@ -0,0 +1,48 @@
+﻿open System
+
+let calculateEvenMedian (array:float[]) =
+    let maxIndex =  float (Array.length array - 1)
+    let lower = floor (maxIndex / 2.)
+    let upper = ceil (maxIndex / 2.)
+    (Array.get array (lower |> int) + Array.get array (upper |> int)) / 2.
+    
+let calculateOddMedian array = 
+    Array.get array (Array.length array / 2)
+
+let calculateMedian array =
+    let floatArray = Array.map(float) array
+    match Array.length array % 2 = 0 with
+    | true -> calculateEvenMedian floatArray
+    | false -> calculateOddMedian floatArray
+
+let splitBySpace (input:string) =
+    input.Split ' '
+
+let main _ = 
+    let lengthOfInput = Console.ReadLine () |> int
+
+    let sortedArray = 
+        Console.ReadLine () 
+        |> splitBySpace 
+        |> Array.map(int)
+        |> Array.sort
+
+    let q2 = calculateMedian sortedArray |> int
+    let q1 =
+        sortedArray
+        |> Array.filter(fun item -> item < q2)
+        |> calculateMedian
+        |> int
+    let q3 =
+        sortedArray
+        |> Array.filter(fun item -> item > q2)
+        |> calculateMedian
+        |> int
+
+    printfn "%i" q1
+    printfn "%i" q2
+    printfn "%i" q3
+    
+    0 // return an integer exit code
+
+main ()
