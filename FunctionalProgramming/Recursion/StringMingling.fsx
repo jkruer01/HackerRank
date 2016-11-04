caf09c216c00cd2b3f11e35ac884b72d194e23ba
Jeremy Kruer <Jeremy.Kruer@adp.com>
2016-11-04 14:40:06 -0400

    Functional Programming - Recursion - String Mingling

---
+++ FunctionalProgramming/Recursion/StringMingling.fsx
@@ -0,0 +1,13 @@
+﻿open System
+
+let main _ = 
+    let string1 = Console.ReadLine ()
+    let string2 = Console.ReadLine ()
+
+    string1
+    |> Seq.zip string2
+    |> Seq.iter(fun (a, b) -> printf "%c%c" b a)
+
+    0 // return an integer exit code
+
+main ()
