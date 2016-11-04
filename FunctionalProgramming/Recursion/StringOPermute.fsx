ff78f505abb73fa147bd598318964914362f7e72
Jeremy Kruer <Jeremy.Kruer@adp.com>
2016-11-04 15:04:53 -0400

    Functional Programming - Recursive - String-o-Permute Initial Solution

---
+++ FunctionalProgramming/Recursion/StringOPermute.fsx
@@ -0,0 +1,15 @@
+﻿open System
+
+let main _ = 
+    let numberOfLines = Console.ReadLine () |> int
+    [|1 .. numberOfLines|]
+    |> Array.map(fun item -> Console.ReadLine().ToCharArray())
+    |> Array.iter(fun item ->
+        item
+        |> Array.chunkBySize 2 
+        |> Array.iter(fun item -> printf "%c%c" item.[1] item.[0])
+        printfn "")
+
+    0 // return an integer exit code
+
+main ()
