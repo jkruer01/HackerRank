385bae096fd39143b7633628b1ebca3c2ac21aed
Jeremy Kruer <Jeremy.Kruer@adp.com>
2016-11-04 09:24:24 -0400

    30 Days of Code - Day 7

---
+++ Tutorials/30DaysOfCode/Day7-Arrays.fsx
@@ -0,0 +1,19 @@
+﻿open System
+
+let splitBySpace (input:string) =
+    input.Split ' '
+
+let readInputs _ = 
+    Console.ReadLine () 
+    |> splitBySpace 
+
+let main _ = 
+    let _ = Console.ReadLine () |> int
+    readInputs () 
+    |> Array.rev
+    |> String.concat " "
+    |> printfn "%s"
+    
+    0 // return an integer exit code
+
+main ()
