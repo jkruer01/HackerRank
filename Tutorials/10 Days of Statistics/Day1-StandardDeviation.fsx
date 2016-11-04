1338c77d2dc5adf4ab17dc080bc1188c18f6f833
Jeremy Kruer <Jeremy.Kruer@adp.com>
2016-11-04 08:57:33 -0400

    Statistics - Day 1: Standard Deviation

---
+++ Tutorials/10 Days of Statistics/Day1-StandardDeviation.fsx
@@ -0,0 +1,33 @@
+﻿open System
+
+let splitBySpace (input:string) =
+    input.Split ' '
+
+let readInputs _ = 
+    Console.ReadLine () 
+    |> splitBySpace 
+    |> Array.map(float)
+
+let calculatedSquaredDistanceFromMean mean number =
+    pown (number - mean) 2
+
+let divide denominator (numerator:float) =
+    numerator / denominator
+
+let standardDeviation numbers =
+    let mean = Array.average numbers
+    numbers
+    |> Array.fold(fun acc num -> acc + calculatedSquaredDistanceFromMean mean num) 0.
+    |> divide (float (Array.length numbers))
+    |> sqrt
+
+
+let main _ = 
+    let _ = Console.ReadLine () |> int
+    let numbers = readInputs ()
+
+    printfn "%.1f" (standardDeviation numbers)
+    
+    0 // return an integer exit code
+
+main ()
