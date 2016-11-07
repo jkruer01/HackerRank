d74509ded88957efcd2f46c8ce2a1b6b2be7e675
Jeremy Kruer <Jeremy.Kruer@adp.com>
2016-11-07 09:46:17 -0500

    Functional Programming - Recursion - String Reductions

---
+++ FunctionalProgramming/Recursion/StringReductions.fsx
@@ -0,0 +1,12 @@
+﻿open System
+
+let main _ = 
+    Console.ReadLine().ToCharArray()
+    |> Array.distinct
+    |> Array.map(string)
+    |> String.concat ""
+    |> printfn "%s"
+
+    0 // return an integer exit code
+
+main ()
