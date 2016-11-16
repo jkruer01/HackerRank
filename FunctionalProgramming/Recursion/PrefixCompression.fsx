69d9ce38e715c9940fb1641d116c5e40cb574223
TeaDrivenDev <TeaDrivenDev@gmail.com>
2016-11-16 03:05:22 +0000

    Reviewed code for three challenges

--- FunctionalProgramming/Recursion/PrefixCompression.fsx
+++ FunctionalProgramming/Recursion/PrefixCompression.fsx
@@ -1,10 +1,32 @@
 open System
 
-let findMatchingPrefix' (input1:string) (input2:string) =
-    input2
-    |> Seq.zip input1
-    |> Seq.takeWhile(fun (a, b) -> a = b)
-    |> Seq.map(fun (a, b) -> a |> string)
+let inline equal (a, b) = a = b
+// `inline` requests inline compilation, that means everywhere `equal` is
+// called, the actual code of the function will appear in the IL instead of
+// a jump.
+// It is only a request, though; as far as I know, the compiler *may*
+// decide against complying with it (presumably for larger functions). 
+
+// The apostrophe is generally used in names when the value/function embodies
+// a slight variation of an already named concept, such as a partially applied
+// "instance" of a more general function, or a value that has been existing
+// previously, but here exists in a form that has already seen additional
+// processing.
+// I also sometimes use the apostrophe for recursive sub-functions within
+// other functions.
+let findMatchingPrefix (input1 : string) (input2 : string) =
+// It's easier to see what's going on with a few spaces around the parameter
+// names and type annotations
+    Seq.zip input1 input2
+    // or
+    // (input1, input2) ||> Seq.zip
+    // either of these makes it clearer that input1 and input2 are "equal"
+    // values being operated on here, rather than `input1` determining
+    // specifics of an operation on `input2`
+    |> Seq.takeWhile equal // reads a little nicer than the lambda
+                               // As long as it's only used once, it doesn't matter
+                               // all that much, though.
+    |> Seq.map (fst >> string) // also a slightly higher-level construct than the lambda
     |> String.concat ""
 
 let printLengthAndContent input =    
@@ -13,7 +35,7 @@ let printLengthAndContent input =
 let main _ = 
     let input1 = Console.ReadLine()
     let input2 = Console.ReadLine()
-    let matchingPrefix = findMatchingPrefix' input1 input2
+    let matchingPrefix = findMatchingPrefix input1 input2
     let prefixLength = String.length matchingPrefix
     
     printLengthAndContent matchingPrefix
--- FunctionalProgramming/Recursion/SuperDigit.fsx
+++ FunctionalProgramming/Recursion/SuperDigit.fsx
@@ -1,37 +1,68 @@
 ﻿open System
 
-let splitBySpace (input:string) =
-    input.Split ' '
+// I generally put such tiny functions on single lines; this saves space and
+// improves the reading flow, because a piece of code such as this function
+// body can get a bit "lost" visually.
+let splitBySpace (input : string) = input.Split ' '
 
-let readInputs _ =
-    let inputs = 
-        Console.ReadLine () 
-        |> splitBySpace 
-    inputs.[0], int64 inputs.[1]
+// While this works with `_`, the placeholder parameter of parameterless
+// functions is by convention always the `unit` value `()`. That makes it
+// immediately clear that there is no useful input to this function, while the
+// `_`  placeholder actually causes the parameter to become generic, which is
+// irritating, because while one can pass *anything*, it will never matter.
+let readInputs () =
+    // Destructuring assignments like this take a bit of getting used to,
+    // but are pretty powerful in that they let you name the individual values
+    // immediately without the need for an intermediate array and "anonymous"
+    // access via "magic number" indices.
+    // Pattern matching in this way incurs a compiler warning, because the
+    // match will fail with an exception for any array that doesn't have
+    // *exactly* two elements.
+    let [| n; k |] = Console.ReadLine () |> splitBySpace 
+    n, int64 k
 
-let calculateP (n, k:int64) =
-    [1L..k]
-    |> List.map(fun _ -> n)
-    |> String.Concat
+// I deleted `calculateP`, because it was not used.)
 
-let sumIndividualDigits (num:string) =
+// This isn't a case where you'd typically use "derivation" naming with an
+// apostrophe; this is really a separate function that might still make sense
+// outside of the function that it's being used from here.
+let sumDigits (num : string) =
     num.ToCharArray()
-    |> Seq.map(string >> int64)
-    |> Seq.sum
+    // Seq.sumBy is the composition of `Seq.map` and `Seq.sum` 
+    |> Seq.sumBy (string >> int64)
     |> string
 
-let sumIndividualDigits' (n, k:int64) =
-    k * (sumIndividualDigits n |> int64)
+// This is a bit less involved and doesn't require making sense of the whole
+// multiplication term at once.
+let sumIndividualDigits n (k : int64) =
+    n
+    |> sumDigits
+    |> int64
+    // Generally (there may be exceptions), operators can be turned into
+    // functions by putting them in parentheses. In this case, `(*)` behaves
+    // like a `multiply` function that takes two factors as parameters.
+    // This behavior can make for more pipe-friendly code as in this example,
+    // however, it should preferably only be used with commutative operations,
+    // because when the order of operands matters, this will read rather
+    // unintuitively.
+    |> (*) k
     |> string 
 
+// Pattern matching on booleans is a little odd; an `if` expression would be
+// more direct. Pattern matching on the actual value is the visually most
+// expressive representation of what this function does.
 let rec calculateSuperDigit num =
-    match (String.length num = 1) with
-    | true -> num
-    | false -> calculateSuperDigit (sumIndividualDigits num)
+    match String.length num with
+    | 1 -> num
+    | _ -> num |> sumDigits |> calculateSuperDigit
 
 let main _ = 
     readInputs()
-    |> sumIndividualDigits'
+    // The ||> operator passes the elements of a 2-tuple to the target
+    // function as two indivdual values, which allows for making the function
+    // more flexible by not requiring its values to be tupled and thus enabling
+    // partial application.
+    ||> sumIndividualDigits
     |> calculateSuperDigit
     |> printfn "%s"
 
